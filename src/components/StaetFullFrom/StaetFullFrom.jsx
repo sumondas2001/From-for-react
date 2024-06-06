c999bb810c1e34d2a3cb210b5997a6c45a9c4d34
sumondas2001 <sumonchandradas1010@gmail.com>
2024-06-06 06:25:21 +0600

    Staet full from explore

--- src/components/StaetFullFrom/StaetFullFrom.jsx
+++ src/components/StaetFullFrom/StaetFullFrom.jsx
@@ -2,30 +2,54 @@ import { useState } from "react";
 
 
 const StaetFullFrom = () => {
-     const [email, setEmail] = useState(null)
+     const [name, setName] = useState(null)
+     const [email, setEmail] = useState(null);
+     const [password, setPassword] = useState(null);
+     const [error, setError] = useState('')
      const handelSubmit = e => {
-          e.preventDefault()
-          console.log(email)
+          e.preventDefault();
+          if (password.length < 6) {
+               setError('Password must be 6 character or longer')
+          }
+          else {
+               setError('')
+               console.log(name, email, password);
+          }
+
 
 
      }
      const handelEmailChange = e => {
-          setEmail(e.target.value)
+          setEmail(e.target.value);
+
+     }
+     const handleNameChange = e => {
+          setName(e.target.value)
+     }
+     const handlePasswordChange = e => {
+          setPassword(e.target.value)
      }
      return (
           <div>
                <form onSubmit={handelSubmit}>
 
-                    <input type="text" name="text" placeholder="name" />
+                    <input
+                         onChange={handleNameChange} required
+                         type="text" name="text" placeholder="name" />
                     <br />
                     <input
-                         onChange={handelEmailChange}
+                         onChange={handelEmailChange} required
                          type="email" name="email" id="" placeholder="Email" />
                     <br />
-                    <input type="password" name="password" id="" placeholder="password" />
+                    <input
+                         onChange={handlePasswordChange} required
+                         type="password" name="password" id="" placeholder="password" />
                     <br /><br />
                     <input type="submit" value="Submit" />
                     <br /><br />
+                    {
+                         error && <p>{error}</p>
+                    }
                </form>
           </div>
      );
